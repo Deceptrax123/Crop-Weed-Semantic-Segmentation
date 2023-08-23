5105f3addf8d51d565248b210bedaf4d3fc76b14
Deceptrax123 <srinitishsri@gmail.com>
2023-08-23 18:31:32 +0530

    data script

--- Weed_dataset.py
+++ Weed_dataset.py
@@ -16,7 +16,7 @@ class WeedDataset(torch.utils.data.Dataset):
         
         sample=self.paths[index]
 
-        red,nir,ndvi,ground=sample[0],sample[1],sample[2],sample[3]
+        ndvi,nir,ground,red=sample[0],sample[1],sample[2],sample[3]
         red_img,nir_img,ndvi_img,ground_img=Image.open(red),Image.open(nir),Image.open(ndvi),Image.open(ground)
         red_np,nir_np,ndvi_np,ground_np=np.array(red_img),np.array(nir_img),np.array(ndvi_img),np.array(ground_img)
 
---
+++ data_script.py
@@ -0,0 +1,62 @@
+
+def read_file():
+    ndvi_train="./data/Sequoia/SequoiaMulti_30/trainNDVI.txt"
+    nir_train="./data/Sequoia/SequoiaMulti_30/trainNir.txt" #Contains annotation too
+    red_train="./data/Sequoia/SequoiaMulti_30/trainRed.txt" 
+
+    ndvi_test="./data/Sequoia/SequoiaMulti_30/testNDVI.txt"
+    nir_test="./data/Sequoia/SequoiaMulti_30/testNir.txt" #Contains annotation too
+    red_test="./data/Sequoia/SequoiaMulti_30/testRed.txt"
+
+    #read each file name and extract arrays
+
+    file_paths=[
+        ndvi_train,nir_train,red_train,ndvi_test,nir_test,red_test
+    ]
+
+    #Train and test split
+
+    train=list()
+    test=list()
+
+    #Train file loop
+    for i in range(125):
+        training_example=list()
+        for j in range(3):
+            f=open(file_paths[j],'r')
+            lines=f.read().splitlines()
+
+            example=lines[i]
+            if j==1:
+                nir,ground=example.split()[0],example.split()[1]
+
+                training_example.append(nir)
+                training_example.append(ground)
+            else:
+                training_example.append(example)
+            
+            f.close()
+        train.append(training_example)
+    
+    #test file loop
+    for i in range(30):
+        testing_example=list()
+        for j in range(3):
+            k=j+3
+
+            f=open(file_paths[k],'r')
+            lines=f.read().splitlines()
+
+            example=lines[i]
+
+            if j==1:
+                nir,ground=example.split()[0],example.split()[1]
+
+                testing_example.append(nir)
+                testing_example.append(ground)
+            else:
+                testing_example.append(example)
+            f.close()
+        test.append(testing_example)
+
+    return train,test
