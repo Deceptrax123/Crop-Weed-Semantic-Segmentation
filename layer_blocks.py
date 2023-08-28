1538d89f1d87ad1ad5a03a2e422c2e2f50901c17
Deceptrax123 <srinitishsri@gmail.com>
2023-08-28 23:16:17 +0530

    add reconstrictor

--- layer_blocks.py
+++ layer_blocks.py
@@ -127,4 +127,52 @@ class Reconsructor(Module):
     def __init__(self):
         super(Reconsructor,self).__init__()
 
-        
+        self.dconv1=ConvTranspose2d(in_channels=256,out_channels=128,stride=2,padding=1,output_padding=1,kernel_size=(3,3))
+        self.bn1=BatchNorm2d(128)
+        self.relu1=ReLU()
+        self.dp1=Dropout2d()
+
+        self.dconv2=ConvTranspose2d(in_channels=128,out_channels=64,padding=1,output_padding=1,kernel_size=(3,3),stride=2)
+        self.bn2=BatchNorm2d(64)
+        self.relu2=ReLU()
+        self.dp2=Dropout2d()
+
+        self.dconv3=ConvTranspose2d(in_channels=64,out_channels=32,kernel_size=(3,3),padding=1,output_padding=1,stride=2)
+        self.bn3=BatchNorm2d(32)
+        self.relu3=ReLU()
+        self.dp3=Dropout2d()
+
+        self.dconv4=ConvTranspose2d(in_channels=32,out_channels=16,kernel_size=(3,3),padding=1,output_padding=1,stride=2)
+        self.bn4=BatchNorm2d(16)
+        self.relu4=ReLU()
+        self.dp4=Dropout2d()
+
+        self.dconv5=ConvTranspose2d(in_channels=16,out_channels=3,stride=2,padding=1,output_padding=1,kernel_size=(3,3))
+
+    def forward(self,x):
+        x=self.dconv1(x)
+        x=self.bn1(x)
+        x=self.relu1(x)
+        x=self.dp1(x)
+
+        x=self.dconv2(x)
+        x=self.bn2(x)
+        x=self.relu2(x)
+        x=self.dp2(x)
+
+        x=self.dconv3(x)
+        x=self.bn3(x)
+        x=self.relu3(x)
+        x=self.dp3(x)
+
+        x=self.dconv4(x)
+        x=self.bn4(x)
+        x=self.relu4(x)
+        x=self.dp4(x)
+
+        x=self.dconv5(x)
+
+        return x
+
+model=Reconsructor()
+summary(model,input_size=(256,32,32),batch_size=8,device='cpu')
