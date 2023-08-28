4da2ec6cf02f4bd27a244f39d319fa293e3cc2f5
Deceptrax123 <srinitishsri@gmail.com>
2023-08-28 22:06:38 +0530

    self encoding complete

--- layer_blocks.py
+++ layer_blocks.py
@@ -1,18 +1,18 @@
 import torch 
-from torch.nn import Conv2d,ConvTranspose2d,ReLU,BatchNorm2d,MaxPool2d,AvgPool2d
+from torch.nn import Conv2d,ConvTranspose2d,ReLU,BatchNorm2d,MaxPool2d,MaxUnpool2d,AdaptiveAvgPool2d
 from torch.nn import Module 
 from torchsummary import summary
 
-class Unet_block(Module):
+class Unet_encoding_block(Module):
     def __init__(self,features):
-        super(Unet_block,self).__init__()
+        super(Unet_encoding_block,self).__init__()
 
         self.conv1=Conv2d(in_channels=features,out_channels=2*features,stride=1,kernel_size=(3,3))
-        self.bn1=BatchNorm2d(features)
+        self.bn1=BatchNorm2d(2*features)
         self.relu1=ReLU()
 
         self.conv2=Conv2d(in_channels=2*features,out_channels=2*features,stride=1,kernel_size=(3,3))
-        self.bn2=BatchNorm2d(features)
+        self.bn2=BatchNorm2d(2*features)
         self.relu2=ReLU()
 
         self.maxpool=MaxPool2d(kernel_size=(2,2),stride=2)
@@ -30,29 +30,58 @@ class Unet_block(Module):
 
         return x
 
-class Self_embedding(Module):
-    def __init__(self):
-        super(Self_embedding,self).__init__()
+class Unet_decoding_block(Module):
+    def __init__(self,features):
+        super(Unet_decoding_block,self).__init__()
 
-        self.bconv1=Conv2d(in_channels=3,out_channels=4,padding=1,stride=2)
-        self.bn1=BatchNorm2d()
+        self.dconv1=ConvTranspose2d(in_channels=features,out_channels=features//2,stride=1,padding=1,output_padding=1,kernel_size=(3,3))
+        self.bn1=BatchNorm2d(features//2)
         self.relu1=ReLU()
 
-        self.bconv2=Conv2d(in_channels=4,out_channels=8,stride=2,padding=1)
-        self.bn2=BatchNorm2d()
+        self.dconv2=ConvTranspose2d(in_channels=features//2,out_channels=features//2,kernel_size=(3,3),padding=1,output_padding=1)
+        self.bn2=BatchNorm2d(features//2)
         self.relu2=ReLU()
 
-        self.bconv3=Conv2d(in_channels=8,out_channels=16,stride=2,padding=1)
-        self.bn3=BatchNorm2d()
+        self.upsample=MaxUnpool2d(kernel_size=(2,2),stride=2)
+
+    def forward(self,x):
+        x=self.dconv1(x)
+        x=self.bn1(x)
+        x=self.relu1(x)
+        
+        x=self.dconv2(x)
+        x=self.bn2(x)
+        x=self.relu2(x)
+
+        x=self.upsample(x)
+
+        return x
+
+class Self_embedding_block(Module):
+    def __init__(self):
+        super(Self_embedding_block,self).__init__()
+
+        self.bconv1=Conv2d(in_channels=3,out_channels=32,stride=2,kernel_size=(3,3))
+        self.bn1=BatchNorm2d(32)
+        self.relu1=ReLU()
+
+        self.bconv2=Conv2d(in_channels=32,out_channels=64,stride=2,kernel_size=(3,3))
+        self.bn2=BatchNorm2d(64)
+        self.relu2=ReLU()
+
+        self.bconv3=Conv2d(in_channels=64,out_channels=128,stride=2,kernel_size=(3,3))
+        self.bn3=BatchNorm2d(128)
         self.relu3=ReLU()
 
-        self.bconv4=Conv2d(in_channels=16,out_channels=32,stride=2,padding=1)
-        self.bn4=BatchNorm2d()
+        self.bconv4=Conv2d(in_channels=128,out_channels=256,stride=2,kernel_size=(3,3))
+        self.bn4=BatchNorm2d(256)
         self.relu4=ReLU()
 
-        self.bconv5=Conv2d(in_channels=16,out_channels=32,stride=2,padding=1)
-        self.bn5=BatchNorm2d()
+        self.bconv5=Conv2d(in_channels=256,out_channels=512,stride=2,kernel_size=(3,3))
+        self.bn5=BatchNorm2d(512)
         self.relu5=ReLU()
+
+        self.adaptive=AdaptiveAvgPool2d(output_size=(31,31))
 
 
     def forward(self,x):
@@ -76,4 +105,10 @@ class Self_embedding(Module):
         x=self.bn5(x)
         x=self.relu5(x)
 
+        x=self.adaptive(x)
+
         return x
+    
+model=Self_embedding_block()
+
+summary(model,input_size=(3,1024,1024),batch_size=8,device='cuda')
