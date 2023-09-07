8a27a5892613f3cb5b90e57665b9dbc958dc0756
Deceptrax123 <srinitishsri@gmail.com>
2023-09-08 01:12:51 +0530

    prepare experiment run models

---
+++ experimental_models/deep_cnn/cnn_block.py
@@ -0,0 +1,37 @@
+import torch 
+from torch.nn import Conv2d,BatchNorm2d,ReLU,Dropout2d,ConvTranspose2d
+from torch.nn import Module 
+
+class Cnn_Block_extractor(Module):
+    def __init__(self,features):
+        super(Cnn_Block_extractor,self).__init__()
+
+        self.conv=Conv2d(in_channels=features,out_channels=features*2,padding=1,kernel_size=(3,3),stride=1)
+        self.bn=BatchNorm2d(features*2)
+        self.dp=Dropout2d()
+        self.relu=ReLU()
+     
+    def forward(self,x):
+        x=self.conv(x)
+        x=self.bn(x)
+        x=self.dp(x)
+        x=self.relu(x)
+
+        return x 
+
+class Cnn_Block_recon(Module):
+    def __init__(self,features):
+        super(Cnn_Block_recon,self).__init__()
+
+        self.conv=ConvTranspose2d(in_channels=features,out_channels=features//2,padding=1,kernel_size=(3,3),output_padding=1)
+        self.bn=BatchNorm2d(features//2)
+        self.dp=Dropout2d()
+        self.relu=ReLU()
+
+    def forward(self,x):
+        x=self.conv(x)
+        x=self.bn(x)
+        x=self.dp(x)
+        x=self.relu(x)
+
+        return x
---
+++ experimental_models/deep_cnn/deep_cnn.py
@@ -0,0 +1,6 @@
+import torch 
+from torch.nn import Conv2d,Dropout2d,ReLU
+from torch.nn import Module 
+from cnn_block import Cnn_Block_extractor,Cnn_Block_recon
+
+
